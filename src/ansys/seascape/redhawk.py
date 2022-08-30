1679740f86bb9e28943a74d8d045c0e5e3e8459e
Maxime Rey <87315832+MaxJPRey@users.noreply.github.com>
2022-08-30 20:50:21 +0200

    Update comment of RedHawkSC.

--- src/ansys/seascape/redhawk.py
+++ src/ansys/seascape/redhawk.py
@@ -4,7 +4,7 @@ from .scapp import SCApp
 # RedHawkSC is same as SCApp for now but can be specialized later
 class RedHawkSC(SCApp):
     """
-    A proxy class to handle the connection to RedHawk-SC installation
+    A proxy class to handle the connection to RedHawk-SC installation.
     """
 
     pass
